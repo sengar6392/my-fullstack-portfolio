eaae5044ea062b3b8ca7076ecaf3a62593dc9a99
Shivam Sengar <shivamsengar995627@gmail.com>
2024-01-09 16:20:00 +0530

    Added NavBar

--- frontend_react/src/App.jsx
+++ frontend_react/src/App.jsx
@@ -1,12 +1,13 @@
 
 import "./App.scss";
+import NavBar from "./components/navbar/NavBar";
 
 function App() {
 
   return (
     <>
       <div className="app">
-
+        <NavBar/>
       </div>
     </>
   );
